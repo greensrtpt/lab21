1492f23d459a5d90159426b98ac107c5bf46a7f8
sireethorn <sireethorn2548.h20p@gmail.com>
2025-02-21 13:47:21 +0700

    average(1).cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
 
+using namespace std;
+
+int main(int argc, char *argv[]){
+    if (argc == 1){
+        cout << "Please input numbers to find average." << endl;
+    }
+    else if (argc > 1){
+        float average = 0;
+        for (int i = 1; i < argc; i++){
+            average += atoi(argv[i]);
+        }
+        average = average / (argc - 1);
+        cout << "---------------------------------"<< endl;
+        cout << "Average of "<<argc -1 <<" numbers = "<< average  << endl;
+        cout << "---------------------------------"<< endl;
+    }
+    return 0;
+}
